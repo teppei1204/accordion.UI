16300c67c31cf73685919695b4e65fd4eb9c391e
teppei1204 <m-n-m09@docomo.ne.jp>
2025-01-24 23:12:41 +0900

    クラス、インスタンス導入var.

--- accordion.js
+++ accordion.js
@@ -1,24 +1,49 @@
 (() =>{
-  const $elm = document.querySelector('#js-accordion')
-  const $trigger = $elm.getElementsByTagName('a');
 
-  const triggerLen = $trigger.length;
-  let index = 0;
-  while (index < triggerLen) {
-    $trigger[index].addEventListener('click', (e) => clickHandler(e));
-    index++;
+  class Accordion {
+      //初期化
+      constructor(obj){
+
+        console.log('obj', obj.hookName);
+
+        const $elm = document.querySelector(obj.hookName)
+        const $trigger = $elm.getElementsByTagName(obj.tagName);
+
+        const triggerLen = $trigger.length;
+        let index = 0;
+        while (index < triggerLen) {
+          $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
+          index++;                                //クラスの中で関数を指定した時はthisを入れなければならない
+        }                                         //this=クラスそのものを参照する クラスの中で関数を呼びたい時はthisを使う
+      }
+
+      //クリックしたら実行される処理
+      clickHandler(e) {
+        e.preventDefault();
+
+        const $target = e.currentTarget;
+        const $content = $target.nextElementSibling;
+        if($content.style.display === 'block'){
+          $content.style.display = 'none';
+        } else {
+          $content.style.display = 'block';
+        }
+    };
   }
+  //インスタンスを生成する時は変数を必ず指定する
+  const fuckingAccordion = new Accordion({
+    hookName: '#js-faq',
+    tagName: 'p'
+  });
 
-  //クリックしたら実行される処理
-  const clickHandler = (e) => {
-    e.preventDefault();
-    
-    const $target = e.currentTarget;
-    const $content = $target.nextElementSibling;
-    if($content.style.display === 'block'){
-      $content.style.display = 'none';
-    } else {
-      $content.style.display = 'block';
-    }
-  };
+  const dummyAccordion = new Accordion({
+    hookName: '#js-accordion',
+    tagName: 'a'
+  });
+
+  const miniAccordion = new Accordion({
+    hookName: '#js-accordion-mini',
+    tagName: 'dt'
+  });
+  
 })();
